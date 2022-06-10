103213da8ce55915ea63b07858ffd57a46f023cf
mojhaid <mojahid319@gmail.com>
2022-06-10 10:27:20 +0600

    design all pages

--- js/script.js
+++ js/script.js
@@ -1,9 +1,41 @@
 $(document).ready(function(){
     $('.menu-toggler').on('click',function(){
+        if(this.scrollY > 500){
+            $('.up').addClass('show');
+        }
+        else{
+            $('.up').removeClass('show');
+        }
         
           $(this).toggleClass('open');
           $('.top-nav').toggleClass('open');
-      
        
-    })
-})
+         
+    });
+   
+
+
+    $('.up').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+
+
+
+    $('.top-nav .nav-link').on('click',function(){
+        
+        $('.menu-toggler').removeClass('open');
+        $('.top-nav').removeClass('open');
+    
+     
+  });
+  
+
+  
+  AOS.init({
+      eassing:'ease',
+      duration:1800
+  });
+  
+});
+
